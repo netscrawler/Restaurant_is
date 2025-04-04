c64840024e789f9ee8646ef6bad9baff5a3661f8
Daniil Epifanov <mariarthyjamesoo@gmail.com>
2025-04-04 22:34:30 +0300

    feat: init

---
+++ scripts/generate_proto.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROTO_ROOT=./proto/v1
+GEN_DIR=./gen
+GO_OPTS="paths=source_relative"
+
+# Скачивание стандартных proto-файлов
+if [ ! -d "./third_party" ]; then
+    git clone --depth 1 https://github.com/protocolbuffers/protobuf.git third_party
+fi
+
+protoc -I=$PROTO_ROOT \
+    -I=./third_party/protobuf/src \
+    --go_out=$GEN_DIR \
+    --go_opt=$GO_OPTS \
+    --go-grpc_out=$GEN_DIR \
+    --go-grpc_opt=$GO_OPTS \
+    $PROTO_ROOT/*.proto

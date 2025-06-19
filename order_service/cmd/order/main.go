93c950a62772acdb47abc5949350ea39b084728c
Daniil Epifanov <mariarthyjamesoo@gmail.com>
2025-06-19 08:13:52 +0300

    fix

--- order_service/cmd/order/main.go
+++ order_service/cmd/order/main.go
@@ -79,6 +79,10 @@ func setupLogger(env string) *slog.Logger {
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
 	}
 
 	return log
